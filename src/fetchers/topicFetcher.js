8a6bd979e15e76c6caccdaafa0a7e15c212dd9b4
Lars Peter Jørgensen <lars-games@outlook.com>
2022-11-29 19:53:11 +0100

    Change baseURL of topicFetcher back to remote.

--- src/fetchers/topicFetcher.js
+++ src/fetchers/topicFetcher.js
@@ -1,7 +1,7 @@
 import { fetchURL, dataFactory } from "./fetcherUtils"
 
-// const baseURL = "http://onebrightcreation.com:8081/CA-3/api/"
-const baseURL = "http://localhost:8080/api/"
+const baseURL = "http://onebrightcreation.com:8081/CA-3/api/"
+// const baseURL = "http://localhost:8080/api/"
 
 async function getTopics() {
   const topics = fetchURL(baseURL+"topic/all")
